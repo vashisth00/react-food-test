27924fdca59541bedc73d81417b63dd4c3f051fe
Vashisth Bhushan <vashisthbhushan@gmail.com>
2022-01-27 03:25:10 +0530

    buttons only

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,18 +1,54 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 
 export default function HeaderTabs() {
+  const [activeTab, setActiveTab] = useState("Delivery");
+console.log('geeg');
   return (
-    <SafeAreaView>
-      <HeaderButton />
+    <SafeAreaView
+      style={{
+        flexDirection: "row",
+        alignSelf: "center",
+        backgroundColor: "black",
+        marginTop: 50,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        borderRadius: 25,
+      }}
+    >
+      <HeaderButton
+        text="Delivery"
+        btnColor="#f5fef7"
+        txtColor="black"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <HeaderButton
+        text="Pickup"
+        btnColor="grey"
+        txtColor="white"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
     </SafeAreaView>
   );
 }
 
-const HeaderButton = () => {
+const HeaderButton = (props) => {
   return (
-    <View>
-      <Text>Delivery</Text>
-    </View>
+    <TouchableOpacity
+      style={{
+        backgroundColor: props.activeTab === props.text ? 'white' : "black",
+        padding: 10,
+        borderRadius: 25,
+      }}
+        onPress={() => props.setActiveTab(props.text)}
+    >
+      <Text style={{ color: props.activeTab === props.text ? 'black' : 'white' }}>{props.text}</Text>
+    </TouchableOpacity>
   );
 };
